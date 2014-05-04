49faefa338ecd2f63bf638f615b201f125876605
someone <sickmind@i2pmail.org>
2014-05-04 18:37:46 +0000

    Workaround for the linux kernel bug where sendmsg returnes EINVAL. These happens because cmsg->cmsg_len is of a wider type in the kernel, and needs investigation there.

--- Network/Socket/Msg/Internal.hs
+++ Network/Socket/Msg/Internal.hs
@@ -3,11 +3,13 @@
 module Network.Socket.Msg.Internal
     ( c_sendmsg
     , c_recvmsg
+    , zeroBytes
     ) where
 
 import Network.Socket.Msg.MsgHdr (MsgHdr)
 
-import Foreign.C.Types (CInt(..))
+import Control.Monad (void)
+import Foreign.C.Types (CInt(..),CSize(..))
 import Foreign.Ptr (Ptr)
 import System.Posix.Types (CSsize(..))
 
@@ -16,3 +18,9 @@ foreign import ccall unsafe "sendmsg"
 
 foreign import ccall unsafe "recvmsg"
   c_recvmsg :: CInt -> Ptr MsgHdr -> CInt -> IO CSsize
+
+foreign import ccall unsafe "memset"
+  c_memset :: Ptr a -> CInt -> CSize -> IO CSize
+
+zeroBytes :: Ptr a -> Int -> IO ()
+zeroBytes p s = void $ c_memset p 0 $ fromIntegral s
